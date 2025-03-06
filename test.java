4b8cb56c6dbff4ff87f09f738229b23f82c173bf
Heshan Pramuditha Dharmasena <rmheshanpramuditha@gmail.com>
2025-03-06 14:54:43 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,20 @@
-git a// SimpleCalculation.java
-
+ // SimpleCalculation.java
 public class SimpleCalculation {
 
-    public static int addTwoNumbers(int a, int b) {      //declare a function to add two numbers
+    public static int addTwoNumbers(int a, int b) {
         return a + b;
     }
 
+    // New feature to subtract two numbers
+    public static int subtractTwoNumbers(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
-        int result = addTwoNumbers(3, 5);
-        System.out.println("The sum is: " + result);
+        int resultAdd = addTwoNumbers(3, 5);
+        int resultSubtract = subtractTwoNumbers(5, 3);
+        System.out.println("Addition result: " + resultAdd);
+        System.out.println("Subtraction result: " + resultSubtract);
     }
 }
+
